32b67a2b50f40b8c4806e99d43aa3dc47d382f10
pazbianca <106789624+pazbianca@users.noreply.github.com>
2022-06-06 22:43:55 -0300

    Update urls.py

--- fashionProject/FashionApp/urls.py
+++ fashionProject/FashionApp/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('fashionBrands', views.fashionBrandsList, name = 'fashionBrands'),
     path('fashionShows', views.fashionShowsList, name = 'fashionShows'),
     path('form_fashionIcons', views.form_fashionIcons),
-    path('form_fashionBrands', views.form_fashionBrands),
+    path('form_fashionBrands', views.form_fashionBrands, name = 'formFashionBrands'),
     path('form_fashionShows', views.form_fashionShows),
     path('form_fashionShows', views.form_fashionShows),
-    path('search_fashionIcons', views.search_fashionIcons),
+    path('search_fashionIcons', views.search_fashionIcons,  name = 'searchFashionIcons'),
     path('searchResults_fashionIcons', views.searchResults_fashionIcons),
 
    
